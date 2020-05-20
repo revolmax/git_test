fdc9ec76ecb7d16b0cec89e0f20ac0e2c4d1931a
revolmax <odraghunov@ukrtelecom.ua>
2020-05-20 16:16:15 +0300

    lesson 1-3

---
+++ app01_03.js
@@ -0,0 +1,66 @@
+/* const firstName ='Oleg'
+//const lastName ='Draghunov'
+let age = 40
+name = 'vasya'
+console.log(firstName);
+//const lastName = prompt ('Insert last name')
+//alert(firstName + '  ' + lastName)
+const firstNumber = 100
+const secondNumber = 200
+//const total = firstNumber - secondNumber
+const a = 10
+const b = 20
+let c  = 25
+c-=a
+c+=b
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(c)
+const firstName ='Oleg'
+const isProg = true
+let age = 40
+name = 'vasya'
+console.log(typeof firstName)
+console.log(typeof isProg)
+console.log(typeof age)
+
+const fullAge = 40
+const currYear = 2020
+const bithYear = 1970
+const isFullAge = currYear - bithYear >= fullAge
+console.log(isFullAge)*/
+/*const work = false
+if (work === 'ready') {
+console.log('go')
+}else if (work === 'pending') {
+console.log('stop')
+} else {
+    console.log('goodby')
+    }
+    work ? console.log('ok') : console.log('bad')
+
+function calcAge(year) {
+    return 2020 - year
+}
+//console.log(calcAge(1960))
+function infoAbout (name, year) {
+
+}
+const cars = ['oka', 'honda', 'bmw']
+for(let car of cars){
+console.log(car)}
+const person = {
+    firstName: 'Oleg',
+    lastName: 'Draghunov',
+    birthYear: 1979,
+    lang: [ 'ru','ua','en'],
+    wife: true,
+    greet: function() {
+        console.log('hi')
+    }
+}
+console.log(person.firstName)
+person.greet()*/
